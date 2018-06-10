06fc2ccdf700a55e0216efc8aabd7689577bc473
root <fr13ndz@protonmail.com>
2018-06-10 12:00:23 +0200

    Add files via upload
    fixed upper and lower boundaries

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 settings = {
-    'trainQuantil': 0.3,
+    'trainQuantil': 0.2,
     'batch_size': 128,
-    'epochs': 500
+    'epochs': 500,
+    'lower_bound_norm': 1e-11,
+    'upper_bound_norm': 1e-3
 }
